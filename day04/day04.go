2173ce5da460d1d5cb884946ebd147c7eee801a2
chwilk <chwilk@gmail.com>
2023-11-22 14:30:08 -0600

    Day 04 part a working

---
+++ day04/day04.go
@@ -0,0 +1,59 @@
+package day04
+
+import (
+	"advent2022-go/helpers"
+	"bufio"
+	"io"
+	"regexp"
+	"strconv"
+)
+
+type Day04 struct {
+}
+
+func (f Day04) Run(input io.Reader, part int) (result int) {
+	switch part {
+	case helpers.PartA:
+		result = PartA(input)
+	default:
+		
+	}
+	return
+}
+
+func PartA(lines io.Reader) (answer int) {
+	scanner := bufio.NewScanner(lines)
+
+	for scanner.Scan() {
+		a, b := parseLine(scanner.Text())
+		if contains(a, b) {
+			answer ++
+		}
+	}
+	return
+}
+
+func contains(a, b [2]int) bool {
+	switch {
+	case a[0] <= b[0] && a[1] >= b[1]:
+		fallthrough
+	case a[0] >= b[0] && a[1] <= b[1]:
+		return true
+	default:
+		return false
+	}
+}
+
+func parseLine(s string) (a, b [2]int) {
+    tokens := regexp.MustCompile("[,-]").Split(s, 4)
+	var err error
+	a[0], err = strconv.Atoi(tokens[0])
+	if err != nil {panic(err)}
+	a[1], err = strconv.Atoi(tokens[1])
+	if err != nil {panic(err)}
+	b[0], err = strconv.Atoi(tokens[2])
+	if err != nil {panic(err)}
+	b[1], err = strconv.Atoi(tokens[3])
+	if err != nil {panic(err)}
+	return
+}
---
+++ day04/day04_test.go
@@ -0,0 +1,64 @@
+package day04
+
+import (
+	"advent2022-go/helpers"
+	"strings"
+	"testing"
+)
+
+const example string = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`
+
+func TestRun(t *testing.T) {
+	t.Run("Run function part A", func(t *testing.T) {
+		expect := 2
+		got := Day04{}.Run(strings.NewReader(example), helpers.PartA)
+		helpers.Check(t, expect, got)
+	})
+	t.Run("Run function part B", func(t *testing.T) {
+		expect := 0
+		got := Day04{}.Run(strings.NewReader(example), helpers.PartB)
+		helpers.Check(t, expect, got)
+	})
+}
+
+func TestContains(t *testing.T) {
+	t.Run("First contains second", func(t *testing.T) {
+		a := [2]int{2, 8}
+		b := [2]int{3, 7}
+		expect := true
+		got := contains(a, b)
+		if expect != got {
+			t.Errorf("Wrong result for %q, %q", a, b)
+		}
+	})
+	t.Run("Second contains first", func(t *testing.T) {
+		a := [2]int{3, 7}
+		b := [2]int{2, 8}
+		expect := true
+		got := contains(a, b)
+		if expect != got {
+			t.Errorf("Wrong result for %q, %q", a, b)
+		}
+	})
+
+}
+
+func TestParseLine(t *testing.T) {
+	s := "12-148,23-367"
+	a, b := parseLine(s)
+	expect := [4]int{12, 148, 23, 367}
+	for i := 0; i < 2; i++ {
+		switch {
+		case a[i] != expect[i]:
+			fallthrough
+		case b[i] != expect[i+2]:
+			t.Errorf("Got %v and %v, expected %v", a, b, expect)
+		default:
+		}
+	}
+}
